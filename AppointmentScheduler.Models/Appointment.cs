3cba3656b4e4e90aa7b8b218181b5ff51bc3fff7
SamithKanshadha <96015238+SamithKanshadha@users.noreply.github.com>
2024-10-16 23:15:49 +0530

    Add files via upload

---
+++ AppointmentScheduler.Models/Appointment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppointmentScheduler.Models
+{
+    public class Appointment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime AppointmentTime { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string AppointmentTitle { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [ForeignKey("ApplicationUserId")]
+        public string ApplicationUserId { get; set; }
+        
+
+    }
+}
---
+++ AppointmentScheduler.Models/ViewModels/AppointmentVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduler.Models.ViewModels
+{
+    public class AppointmentVM
+    {
+        public int Id { get; set; }
+        public string AppointmentTitle { get; set; }
+        public DateTime AppointmentTime { get; set; }
+        public string Description { get; set; }
+    }
+}
